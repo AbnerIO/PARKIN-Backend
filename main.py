ecaaf7a78405e47244fb92d9541151986d9f69d1
Abner Velázquez <64109505+AbnerIO@users.noreply.github.com>
2023-06-12 16:42:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-import requests
 
 app = Flask(__name__)
 
